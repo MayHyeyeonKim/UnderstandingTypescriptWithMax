f91ffa1eecb28dbbe16187bc42b8be7f89b6ec8a
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-25 23:50:06 -0700

    type

---
+++ 02-TypeScriptBasics&BasicTypes/basics.ts
@@ -0,0 +1,6 @@
+let userName: string
+let firstName: string = "May";
+let userAge = 38;
+
+// console.log(userName); // ⚠️ Error in strict mode: Variable 'userName' is used before being assigned.
+console.log(firstName); // May
