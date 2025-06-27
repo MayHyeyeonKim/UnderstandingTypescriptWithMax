5e1ec339ee1d500fedf5edb3a409a431f9822760
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-26 21:09:48 -0700

    type annotation, default value assignment, and both

--- 02-TypeScriptBasics&BasicTypes/basics.ts
+++ 02-TypeScriptBasics&BasicTypes/basics.ts
@@ -4,3 +4,16 @@ let userAge = 38;
 
 // console.log(userName); // ⚠️ Error in strict mode: Variable 'userName' is used before being assigned.
 console.log(firstName); // May
+
+// function add(a: number, b = 5) {
+//     return a + b;
+// }
+
+function add(a: number, b: number = 5) {
+    return a + b;
+}
+let result1 = add(10)
+let result2 = add(10, 20); // 30
+
+console.log(result1); // 15
+console.log(result2); // 30
