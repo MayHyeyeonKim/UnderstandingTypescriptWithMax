2d2791bb8f3d0dae9d76c1ba044a6a6493b79aaa
May Kim <devmay202@gmail.com>
2025-07-27 20:53:38 -0700

    chore

--- src/11-ECMAScriptDecorators/139_145.ts
+++ src/11-ECMAScriptDecorators/139_145.ts
@@ -1,65 +1,65 @@
-// 139. Creating a Method Decorator
-// 140. Using Decorators To Solve A Common Problem
-// 141. Implementing A Decorator-based Solution: autobind
-// 142. Replacing Methods with Decorators
-// 143. Introducing the Field Decorator
-// 144. Building Configurable Decorators with Factories
-// 145. Onwards to Experimental Decorators
+// // 139. Creating a Method Decorator
+// // 140. Using Decorators To Solve A Common Problem
+// // 141. Implementing A Decorator-based Solution: autobind
+// // 142. Replacing Methods with Decorators
+// // 143. Introducing the Field Decorator
+// // 144. Building Configurable Decorators with Factories
+// // 145. Onwards to Experimental Decorators
 
-function logger139<T extends new (...args: any[]) => any>(target: T) {
-  console.log("logger decorator");
-  console.log(target);
+// function logger139<T extends new (...args: any[]) => any>(target: T) {
+//   console.log("logger decorator");
+//   console.log(target);
 
-  return class extends target {
-    constructor(...args: any[]) {
-      super(...args);
-      console.log("class constructor");
-      console.log(this);
-    }
-  };
-}
-
-// custom method decorator
-function autobind(
-  target: (...args: any[]) => any,
-  ctx: ClassMethodDecoratorContext
-) {
-  ctx.addInitializer(function (this: any) {
-    this[ctx.name] = this[ctx.name].bind(this);
-  });
-
-  return function (this: any) {
-    console.log("Executing original function");
-    target.apply(this);
-  };
-}
-
-// custom field decorator factory
-function replacer<T>(initValue: T) {
-  return function replacerDecorator(
-    target: undefined,
-    context: ClassFieldDecoratorContext
-  ) {
-    console.log(target);
-    console.log(context);
-
-    context.addInitializer(function () {
-      (this as any)[context.name] = initValue;
-    });
-  };
-}
-
-// @logger139
-// class Person139 {
-//   @replacer("")
-//   name = "May";
-
-//   @autobind
-//   greet() {
-//     console.log("Hi, I am " + this.name);
-//   }
+//   return class extends target {
+//     constructor(...args: any[]) {
+//       super(...args);
+//       console.log("class constructor");
+//       console.log(this);
+//     }
+//   };
 // }
 
-// const may = new Person139();
-// const greet139 = may.greet;
-// greet139();
+// // custom method decorator
+// function autobind(
+//   target: (...args: any[]) => any,
+//   ctx: ClassMethodDecoratorContext
+// ) {
+//   ctx.addInitializer(function (this: any) {
+//     this[ctx.name] = this[ctx.name].bind(this);
+//   });
+
+//   return function (this: any) {
+//     console.log("Executing original function");
+//     target.apply(this);
+//   };
+// }
+
+// // custom field decorator factory
+// function replacer<T>(initValue: T) {
+//   return function replacerDecorator(
+//     target: undefined,
+//     context: ClassFieldDecoratorContext
+//   ) {
+//     console.log(target);
+//     console.log(context);
+
+//     context.addInitializer(function () {
+//       (this as any)[context.name] = initValue;
+//     });
+//   };
+// }
+
+// // @logger139
+// // class Person139 {
+// //   @replacer("")
+// //   name = "May";
+
+// //   @autobind
+// //   greet() {
+// //     console.log("Hi, I am " + this.name);
+// //   }
+// // }
+
+// // const may = new Person139();
+// // const greet139 = may.greet;
+// // greet139();
