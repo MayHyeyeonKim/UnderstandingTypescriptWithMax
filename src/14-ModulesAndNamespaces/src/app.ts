71c824ca28d99c33e30bfec563c413a2705f97fc
May Kim <devmay202@gmail.com>
2025-08-01 19:46:33 -0700

    14 - Modules & Namespaces

---
+++ src/14-ModulesAndNamespaces/src/app.ts
@@ -0,0 +1,6 @@
+import { ProjectInput } from "./components/project-input.ts";
+import { ProjectList } from "./components/project-list.ts";
+
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
